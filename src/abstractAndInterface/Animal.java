4a16111bda586d692b6f50813c4027f0c303aa17
Saisha <vsaisha1712@gmail.com>
2022-03-25 17:43:34 +0530

    A comment is added in a animal class

--- src/abstractAndInterface/Animal.java
+++ src/abstractAndInterface/Animal.java
@@ -2,9 +2,14 @@ package abstractAndInterface;
 
 public abstract class Animal { // Abstract Class
 	
+	/** 
+	 * This method is roaring
+	 */
 	public void Roaring() { //Non-Abstract method
 		System.out.println("I can Roar");
 	}
 	public abstract void walking(); // Abstract method
 	
 }
+
+
